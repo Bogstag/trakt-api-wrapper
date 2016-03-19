78a538f8b7efc38ce9a0a3af7cdb5123c0adef9f
Bogstag <krister@bogstag.se>
2016-03-19 21:10:52 +0100

    forgot to uncomment

--- src/Wubs/Trakt/Trakt.php
+++ src/Wubs/Trakt/Trakt.php
@@ -129,17 +129,17 @@ class Trakt
         $id = $this->auth->provider->getClientId();
         $this->calendars = new Calendars($id, $this->client);
         $this->checkIn = new CheckIn($id, $this->client);
-  //      $this->comments = new Comments($id, $this->client);
+        $this->comments = new Comments($id, $this->client);
         $this->episodes = new Episodes($id, $this->client);
         $this->genres = new Genres($id, $this->client);
         $this->movies = new Movies($id, $this->client);
         $this->people = new People($id, $this->client);
-   //     $this->recommendations = new Recommendations($id, $this->client);
+        $this->recommendations = new Recommendations($id, $this->client);
         $this->scrobble = new Scrobble($id, $this->client);
         $this->search = new Search($id, $this->client);
         $this->seasons = new Seasons($id, $this->client);
-    //    $this->shows = new Shows($id, $this->client);
-    //    $this->sync = new Sync($id, $this->client);
-    //    $this->users = new Users($id, $this->client);
+        $this->shows = new Shows($id, $this->client);
+        $this->sync = new Sync($id, $this->client);
+        $this->users = new Users($id, $this->client);
     }
 }
